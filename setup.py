cedc8acbc90c621bc3a934458ba0abfb013d18df
David Elliott <delliott537@gmail.com>
2020-04-03 14:20:30 -0400

    Upload to PyPi

--- setup.py
+++ setup.py
@@ -5,14 +5,16 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="jupyter-live-graph", # Replace with your own username
-    version="1.0.0",
+    version="0.1.0",
     author="David E",
     author_email="delliott537@gmail.com",
     description="Create and update graphs live in Jupyter",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    license='MIT',
     url="https://dav-ell.github.io/jupyter-live-graph",
     packages=setuptools.find_packages(),
+    install_requires=['matplotlib'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
